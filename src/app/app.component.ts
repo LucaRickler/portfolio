8d9f146da872fc5b7e0fde14043bce2404ba6e6a
Luca Rickler <luca.rickler@gmail.com>
2018-07-07 17:05:37 +0200

    TranslateAnimation
    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { fadeAnimation } from './animations/fade.animation';
+import { translateAnimation } from './animations/translate.animation';
 
 @Component({
   selector: 'angular-root',
-  animations: [ fadeAnimation ],
+  animations: [ translateAnimation, fadeAnimation ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
