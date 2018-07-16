35c6016dd578a8c234bfc5cce516524b3a752dfc
Luca Rickler <luca.rickler@gmail.com>
2018-07-16 23:17:29 +0200

    PortfolioElementComponent basics

--- src/app/portfolio/PORTFOLIO.ts
+++ src/app/portfolio/PORTFOLIO.ts
@@ -1,7 +1,21 @@
 import { PortfolioElement } from './portfolio.element';
 
 export const ELEMENTS: PortfolioElement[] = [
-  new PortfolioElement('MathDungeon', [], '', [], 'Videogames'),
+  new PortfolioElement(
+    'MathDungeon', [
+      'Unity',
+      'C#',
+      'Android',
+      'iOS',
+      'Windows',
+      'Mac',
+'      Linux'
+    ],
+    // tslint:disable-next-line:max-line-length
+    '<p>An educational video game build to allow elementary-school kids to exercise on basic arithmetics computations.</p><p>I worked as the main programmer for the project. The game features a basic post-processing effect for Sobel filtering, also done by me (not active on every device because of framerate issues).</p><p>Link: <a href="http://mathdungeon.napsygames.com" target="_blank">mathdungeon.napsygames.com</a></p>',
+    ['Screenshot_5_16.9.png', 'Screenshot_3_16.9b_small.png'],
+    'Videogames'
+  ),
   new PortfolioElement('', [], '', [], '')
 ];
 
