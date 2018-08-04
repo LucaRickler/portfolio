1f4fd7a30565cdb534a7f758fa18d79e79bf6640
Luca Rickler <luca.rickler@gmail.com>
2018-08-04 18:53:29 +0200

    Google Analytics

--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { fadeAnimation } from '../shared/animations/fade.animation';
 import { translateAnimation } from '../shared/animations/translate.animation';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'angular-root',
@@ -10,4 +11,12 @@ import { translateAnimation } from '../shared/animations/translate.animation';
 })
 export class CoreComponent {
   title = 'angular';
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+     if (event instanceof NavigationEnd) {
+       (<any>window).ga('set', 'page', event.urlAfterRedirects);
+       (<any>window).ga('send', 'pageview');
+     }
+   });
+ }
 }
