8bb5a03ec129dab129b21182ad055e148844797e
Luca Rickler <luca.rickler@gmail.com>
2026-02-26 10:45:49 +0100

    fix links

--- src/app/core/landing/landing.component.ts
+++ src/app/core/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
@@ -6,7 +7,7 @@ import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
   selector: 'angular-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterLink],
 })
 export class LandingComponent implements OnInit {
 
--- src/app/core/welcome/welcome.component.ts
+++ src/app/core/welcome/welcome.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'angular-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
+  imports: [RouterLink]
 })
 export class WelcomeComponent implements OnInit {
 
